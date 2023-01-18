cfaa81c26e6b6a785aa813c524ee2df18164dce5
kyeongmin <kyeongmin@kyeongminui-MacBookPro.local>
2023-01-18 15:17:12 +0900

    first commit

--- src/RomanNumber.java
+++ src/RomanNumber.java
@@ -5,9 +5,36 @@ public class RomanNumber {
     }
 
     private String toRomanNumber(int decimalNumber) {
-        // TODO: 구현하세요.
-        return "I";
+        String[] romanStrings = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int[] decimalArabic = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+
+        validate(decimalNumber);
+
+        StringBuilder result = new StringBuilder();
+        int index = romanStrings.length -1;
+        while(index > -1) {
+
+            if(decimalNumber/decimalArabic[index] <= 0) {
+                index--;
+                continue;
+            }
+
+            for(int i=0; i< decimalNumber/decimalArabic[index]; i++) {
+                result.append(romanStrings[index]);
+            }
+            decimalNumber %= decimalArabic[index];
+            index--;
+        }
+
+        return result.toString();
     }
+
+    private void validate(int decimalNumber) {
+        if(decimalNumber < 1 || 3999 < decimalNumber) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public String value() {
         return this.romanNumber;
     }
--- test/RomanNumberTest.java
+++ test/RomanNumberTest.java
@@ -4,11 +4,59 @@ import static org.junit.Assert.assertEquals;
 
 public class RomanNumberTest {
     @Test
-    public void romanNumberValueShouldGetRomanNumber() {
+    public void romanNumberValueShouldGetRomanNumber_1() {
         RomanNumber one = new RomanNumber(1);
         assertEquals("I", one.value());
-        //TODO: 테스트를 추가하세요.
+
+        RomanNumber two = new RomanNumber(2);
+        assertEquals("II", two.value());
+
+        RomanNumber three = new RomanNumber(3);
+        assertEquals("III", three.value());
+
+        RomanNumber four = new RomanNumber(4);
+        assertEquals("IV", four.value());
     }
 
-    //TODO: 테스트를 추가하세요.
+    @Test
+    public void romanNumberValueShouldGetRomanNumber_2() {
+        RomanNumber one = new RomanNumber(10);
+        assertEquals("X", one.value());
+
+        RomanNumber two = new RomanNumber(49);
+        assertEquals("XLIX", two.value());
+
+        RomanNumber three = new RomanNumber(88);
+        assertEquals("LXXXVIII", three.value());
+    }
+
+    @Test
+    public void romanNumberValueShouldGetRomanNumber_3() {
+        RomanNumber one = new RomanNumber(145);
+        assertEquals("CXLV", one.value());
+
+        RomanNumber two = new RomanNumber(467);
+        assertEquals("CDLXVII", two.value());
+
+        RomanNumber three = new RomanNumber(758);
+        assertEquals("DCCLVIII", three.value());
+    }
+
+    @Test
+    public void romanNumberValueShouldGetRomanNumber_4() {
+        RomanNumber one = new RomanNumber(1000);
+        assertEquals("M", one.value());
+
+        RomanNumber two = new RomanNumber(1003);
+        assertEquals("MIII", two.value());
+
+        RomanNumber three = new RomanNumber(3999);
+        assertEquals("MMMCMXCIX", three.value());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void arabicNumberValueInputCheck_exception() {
+        RomanNumber romanNumber = new RomanNumber(4000);
+        romanNumber.value();
+    }
 }
